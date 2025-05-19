da601b84b7742fc50ec81b8526c717ac526d6275
Alisa Vishniakova <vishnyakova.alisa@gmail.com>
2025-05-20 01:17:28 +0700

    Kubernetis and Helm files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Создаем HTML с логами
+
 generate_html() {
   echo "<!DOCTYPE html>
 <html>
 <head>
   <title>App Logs</title>
-  <meta http-equiv=\"refresh\" content=\"5\">
+  <meta http-equiv="refresh" content="5">
   <style>
     body { font-family: Arial; margin: 20px; }
     pre { background: #f4f4f4; padding: 10px; }
@@ -14,18 +14,21 @@ generate_html() {
 </head>
 <body>
   <h1>Application Logs</h1>
-  <pre>$(cat /usr/share/nginx/html/app.log | sed 's/</\&lt;/g; s/>/\&gt;/g')</pre>
+  <pre>$(cat /usr/share/nginx/html/app.log | sed 's/</&lt;/g; s/>/&gt;/g')</pre>
 </body>
 </html>"
 }
 
-# Обновляем логи каждые 5 секунд
+echo test
 while true; do
-  #docker logs  lab_upprpo-app-1
-  docker logs  lab_upprpo-app-1 > /usr/share/nginx/html/app.log
+  
+  # Получаем логи из пода и сохраняем их в файл
+  kubectl logs deployment/app-upp > /usr/share/nginx/html/app.log
+  
+  # Генерируем HTML с логами
   generate_html > /usr/share/nginx/html/index.html
+  
   sleep 5
 done &
 
 exec nginx -g 'daemon off;'
-
